6919f752afacf35d75a2d1c21f1df3f639cc8045
SatyaP <stylearning7@gmail.com>
2022-10-13 14:52:49 +0700

    setting news API and make a template for views

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\homeController;
 use App\Http\Controllers\newsController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,7 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/news', [newsController::class,"index"]);
+Route::get('/', [homeController::class,"index"]);
